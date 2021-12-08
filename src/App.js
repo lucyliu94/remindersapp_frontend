8e7ef82bfbd3bb78d91f74d9939cf592f3fad6f1
Lucy Liu <lucy.liu94@gmail.com>
2021-12-08 11:36:19 -0500

    singular routes working

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-import {Route, Routes} from "react-router-dom";
+import {Route, Routes, Link, useNavigate} from "react-router-dom";
 import {useState, useEffect} from "react";
 import AllReminders from "./pages/AllReminders"
+import SingleReminder from './pages/SingleReminder';
+import Form from './pages/Form';
 
 function App() {
+
+  const navigate = useNavigate()
 
   //api 
   const url = "https://remindersapp-backend-ll.herokuapp.com/reminders/";
 
   //state to hold all the reminders
   const [reminders, setReminders] = useState([]);
+
+  //initializing the form here
+  const nullReminder = {
+    title:"",
+    details: ""
+  }
 
 //  Function to get list of REMINDERS from api
 const getReminders = async () => {
@@ -23,16 +33,37 @@ useEffect(() => {
   getReminders();
 }, []);
 
+////////
+//ALL FUNCTIONS
+////////
+const addReminders = async(newReminder) => {
+  const response = await fetch(url, {
+    method:"post",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(newReminder)
+  });
+  getReminders();
+};
+
+
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<AllReminders reminders={reminders}/>}/>
-        <Route path="/reminder/id"/>
-        <Route path="/new"/>
+        <Route path="/reminders/:id" element={<SingleReminder 
+        reminders={reminders} 
+        // edit={getTargetReminder}
+        // deleteReminder={deleteReminder}
+        />} />
+        <Route path="/new" element={<Form
+        initialReminder={nullReminder}
+        handleSubmit={addReminders}
+        buttonLabel="Create New Reminder"
+        />}/>
         <Route path="edit"/>
       </Routes>
-
-
 
     </div>
   );
--- src/pages/SingleReminder.jsx
+++ src/pages/SingleReminder.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import { Link, useParams } from 'react-router-dom'
 
-const SingleReminder = () => {
-    return <h1>Single Reminder</h1>
+const SingleReminder = ({reminders, edit, deleteReminder}) => {
+    const params = useParams()
+    const id = parseInt(params.id)
+    const reminder = reminders.find((r) => r.id === id)
+    console.log(reminder)
+    
+    return <div>
+        <h1>{reminder?.title}</h1>
+        <h2>{reminder?.details}</h2>
+        <button onClick={() => edit(reminder)}>Edit</button>
+        <button onClick={() => deleteReminder(reminder)}>Delete</button>
+        <Link to="/">
+            <button>Go Back</button>
+        </Link>
+
+    </div>
 }
 
 export default SingleReminder
